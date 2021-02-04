281c0191a9a183b927ceddf8708f3cdec359683f
ohveloper <ohveloper@gmail.com>
2021-02-04 23:42:34 +0900

    Thur

--- 07_dfs/21_02_03.js
+++ 07_dfs/21_02_03.js
@@ -4,6 +4,23 @@ let dfs = function (node) {
     value = value.concat(dfs(n));
   });
   return value;
+  //? 처음 푼 코드
+  // 해당 노드에 칠드런이 있는지 검사한다
+  // 있으면 해당 칠드런을 대상으로 재귀
+  // 없으면 해당 벨류 새로운 배열에 푸쉬
+  // 배열 리턴
+  // let result = []
+  // let recur = function(currNode) {
+  //   result.push(currNode.value)
+  //   if (currNode.children.length) {
+  //     for (let i of currNode.children) {
+  //       recur(i)
+  //     }
+  //   }
+
+  // }
+  // recur(node)
+  // return result
 };
 
 // 이 아래 코드는 변경하지 않아도 됩니다. 자유롭게 참고하세요.
